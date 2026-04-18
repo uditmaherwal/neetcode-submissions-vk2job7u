c2242b8138fa659fa1e8348232df363185c12b8b
Udit Maherwal <51200625+uditmaherwal@users.noreply.github.com>
2026-04-18 12:31:55 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = 0;
+        int sell = 1;
+        int maxProfit = 0;
+
+        while(sell < prices.length){
+            if(prices[buy] < prices[sell]){
+                maxProfit = Math.max(maxProfit, prices[sell] - prices[buy]);
+            }else{
+                buy = sell;
+            }
+            sell++;
+        }
+        return maxProfit;
+    }
+}
