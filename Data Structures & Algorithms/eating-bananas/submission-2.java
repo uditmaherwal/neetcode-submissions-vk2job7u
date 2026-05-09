0e28705346acc204871f0417b74beecde761ea47
Udit Maherwal <51200625+uditmaherwal@users.noreply.github.com>
2026-05-09 09:19:58 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = Arrays.stream(piles).max().getAsInt();
+        int res = right;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long totalTime = 0;
+
+            for(int pile : piles){
+                totalTime += Math.ceil((double) pile/mid);
+            }
+
+            if(totalTime <= h){
+                res = mid;
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+}
