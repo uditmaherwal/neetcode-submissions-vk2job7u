0df6ea37317401ee0dfa00dbe779214d6d94ebc2
Udit Maherwal <51200625+uditmaherwal@users.noreply.github.com>
2026-05-18 20:16:50 +0530

    Add: counting-bits - submission-2

---
+++ Data Structures & Algorithms/counting-bits/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] res = new int[n+1];
+        if(n == 0){
+            return res;
+        }
+
+        for(int i = 1; i <= n; i++){
+            if(i%2 == 0){
+                res[i] = res[i/2];
+            }else{
+                res[i] = res[i/2] + 1;
+            }
+        }
+        return res;
+    }
+}
