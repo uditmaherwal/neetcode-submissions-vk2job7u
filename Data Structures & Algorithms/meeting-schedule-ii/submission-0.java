31d18225353231b87d2c4408bae498e38b362473
Udit Maherwal <51200625+uditmaherwal@users.noreply.github.com>
2026-05-23 10:23:08 +0530

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.java
@@ -0,0 +1,31 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        if(intervals == null || intervals.size() == 0 ) return 0;
+
+        intervals.sort((a,b) -> a.start - b.start);
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for(Interval interval : intervals){
+            if (!minHeap.isEmpty() && minHeap.peek() <= interval.start) {
+
+                minHeap.poll();
+
+            }
+
+            minHeap.add(interval.end);
+        }
+        return minHeap.size();
+    }
+}
